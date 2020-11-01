b4470fc69dc92c760954e03ce110e6daa3031301
antoniopan <franciscojavieryeste@hotmail.com>
2020-11-01 12:10:26 +0800

    Update exercise01.py

--- IntroductionToDeepLearning/Exercise01/exercise01.py
+++ IntroductionToDeepLearning/Exercise01/exercise01.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
 
 
 def train_surface():
